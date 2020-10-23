7991f6885a68096eaaa2434557b0cffee93ea3b8
zlongyue <zlongyue@126.com>
2020-10-23 15:12:23 +0800

    Update MysqlReader.java

--- mysqlreader/src/main/java/com/alibaba/datax/plugin/reader/mysqlreader/MysqlReader.java
+++ mysqlreader/src/main/java/com/alibaba/datax/plugin/reader/mysqlreader/MysqlReader.java
@@ -27,9 +27,9 @@ public class MysqlReader extends Reader {
             this.originalConfig = super.getPluginJobConf();
 
             Integer userConfigedFetchSize = this.originalConfig.getInt(Constant.FETCH_SIZE);
-            if (userConfigedFetchSize != null) {
-                LOG.warn("对 mysqlreader 不需要配置 fetchSize, mysqlreader 将会忽略这项配置. 如果您不想再看到此警告,请去除fetchSize 配置.");
-            }
+           // if (userConfigedFetchSize != null) {
+           //     LOG.warn("对 mysqlreader 不需要配置 fetchSize, mysqlreader 将会忽略这项配置. 如果您不想再看到此警告,请去除fetchSize 配置.");
+           // }
 
             this.originalConfig.set(Constant.FETCH_SIZE, userConfigedFetchSize);
 
